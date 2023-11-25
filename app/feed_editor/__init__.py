3b8e7c6d671411bb34f6bf3bee2ecd5b1dbcde21
Glossawy <glossawy@sphorb.email>
2023-11-25 13:48:32 -0500

    Enable CORS by default

--- app/feed_editor/__init__.py
+++ app/feed_editor/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, Blueprint
 from flask_cors import CORS
 
@@ -8,7 +10,10 @@ import feed_editor.health_check as health_check
 
 def create_app() -> Flask:
     app = Flask(__name__, instance_relative_config=True)
-    CORS(app)
+
+    # Allow all for CORS only if env var set
+    if os.environ.get("APP_DISABLE_CORS", "0") == "1":
+        CORS(app)
 
     blueprints: list[Blueprint] = [
         rewrite.blueprint,
