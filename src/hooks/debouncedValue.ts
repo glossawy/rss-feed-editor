44a20116bb8b846fc7a168baa1c37b7fd10206be
Glossawy <glossawy@sphorb.email>
2024-03-31 01:11:13 -0400

    Remove mounted check, dont think it is necessary

--- src/hooks/debouncedValue.ts
+++ src/hooks/debouncedValue.ts
@@ -2,7 +2,6 @@ import { useCallback, useEffect, useRef, useState } from "react"
 
 export function useDebouncedValue<T>(value: T, wait: number) {
   const [internalValue, setInternalValue] = useState(value)
-  const isMountedRef = useRef(false)
   const timeoutRef = useRef<NodeJS.Timeout | null>(null)
 
   const cancel = useCallback(() => {
@@ -11,18 +10,12 @@ export function useDebouncedValue<T>(value: T, wait: number) {
   console.log("debounced")
 
   useEffect(() => {
-    if (!isMountedRef.current) return
-
     cancel()
 
     timeoutRef.current = setTimeout(() => {
       setInternalValue(value)
     }, wait)
   }, [cancel, value, wait])
-
-  useEffect(() => {
-    isMountedRef.current = true
-  }, [])
 
   return [internalValue, cancel] as const
 }
